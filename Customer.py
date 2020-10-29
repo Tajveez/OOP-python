33f3665b82c82e4f0763134fa59046b4c4fb114e
tajveez <tajvee786@gmail.com>
2020-10-29 11:03:56 -0700

    New Changes

--- Customer.py
+++ Customer.py
@@ -2,6 +2,9 @@ class Customer:
     def __init__(self, name, membership_type):
         self.name = name
         self.membership_type = membership_type
+    
+    def update_membership(self, new_membership):
+        self.membership_type = new_membership
 
 c = Customer("Tajveez", "Premium")
 # print(c.name, c.membership_type)
@@ -11,9 +14,10 @@ c2 = Customer("Hamza", "SuperPremium")
 
 customers = [c, c2]
 print(customers[0].name)
-
-# Dynamic attributes adding
-customers[1].verified = True
-print(customers[1].verified)
+customers[0].update_membership("Gold")
+print(customers[0].membership_type)
+# # Dynamic attributes adding
+# customers[1].verified = True
+# print(customers[1].verified)
 
 # https://youtu.be/MikphENIrOo?t=1089
